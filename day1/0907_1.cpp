3a8d6b11ecfbb6cbb4c9f373cdf20264af4ab913
humingliang <37968924+humingliang@users.noreply.github.com>
2020-09-12 19:02:27 +0800

    hello world

---
+++ day1/0907_1.cpp
@@ -0,0 +1,10 @@
+#include"iostream"//包含C++的头文件
+
+using namespace std;//标准命名空间std
+
+int main()
+{
+    printf("hello....\n");//输出
+    cout<<"hello..."<<endl;//<<左移操作符进行功能的改造——操作符重载
+    system("pause");
+}
